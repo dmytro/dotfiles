0c77a1993edea1311c07f321bd954c63d4c4c4cf
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-05-27 21:39:00 +0900

    ssh changes

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -4,8 +4,8 @@ HISTSIZE=10000
 SAVEHIST=10000
 
 setopt hist_ignore_all_dups # don't store duplicates in history at all, even if they are *not* typed in a row (as opposed to hist_ignore_dups)
-setopt share_history
+# setopt share_history
 setopt hist_verify # don't run the command from history immediately, rather wait for another "enter" hit
-setopt inc_append_history
+# setopt inc_append_history
 setopt extended_history
 setopt hist_expire_dups_first # when the history file becomes full, start removing duplicates first
