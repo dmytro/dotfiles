47e4d4fca2d4cd9327f34520fce0b960bda4ea0b
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-11-22 18:48:26 +0900

    zsh git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,3 +6,17 @@
 #prompt redhat
 PS1="[%n@%m %1~]$ "
 
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+precmd() {
+    vcs_info
+}
+
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-changes true
+
+zstyle ':vcs_info:git*' formats "%s:%r/%S -%b- %F{5}%a %m%u%c"
+zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+
+PS1='${vcs_info_msg_0_}%f 
+%n@%m:%1~ $ '
