991293de007ce4d77c0a39c801ad0a6db260faba
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2014-06-06 12:40:01 +0900

    if java home exist

--- zsh/ec2.zsh
+++ zsh/ec2.zsh
@@ -1,3 +1,3 @@
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+[ -d /usr/libexec/java_home ] && export JAVA_HOME="$(/usr/libexec/java_home)"
+[ -d /usr/libexec/java_home ] && export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
 
