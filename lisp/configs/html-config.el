84f9a4e3a28d7e581228313c0a438c2a5fd1853a
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2016-08-12 14:20:35 +0900

    Split files

---
+++ lisp/configs/html-config.el
@@ -0,0 +1,9 @@
+;;;
+;;; HTML helper
+;;;
+;; (autoload 'html-helper-mode "html-helper-mode" "Yay HTML" t)
+(setq auto-mode-alist (cons '("\\.html$" . html-mode) auto-mode-alist))
+;; (setq html-helper-do-write-file-hooks t)
+;; (setq html-helper-build-new-buffer t)
+;; (setq html-helper-use-expert-menu 't) ; full menu
+					;Apr 21 1999
